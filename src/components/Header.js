a47ee9864028a1b5f3a166e5e5355061397470fa
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-05-08 18:31:04 -0700

    update: header format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import heroImage from "../images/books-2.jpg";
 
-const headerStyle = {
-//   "backgroundImage": `url(${heroImage})`,
-  "maxWidth": "100vw",
-  height: "calc(100vw * .43333)",
-  "backgroundSize": "cover",
-  position: "relative"
-}
+// const headerStyle = {
+// //   "backgroundImage": `url(${heroImage})`,
+//   "maxWidth": "100vw",
+//   height: "calc(100vw * .43333)",
+//   "backgroundSize": "cover",
+//   position: "relative"
+// }
 
 function Header() {
     return (
